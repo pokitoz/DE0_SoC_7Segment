c1d5d37a03ee59c4dd52832246c70ef2298eda24
pokitoz <florian.depraz@epfl.ch>
2016-12-14 15:26:07 +0100

    SW version to modify the clock rate for the display

--- sw/nios/application/Display_Segment/display_segment.c
+++ sw/nios/application/Display_Segment/display_segment.c
@@ -1,11 +1,22 @@
 #include "sys/alt_stdio.h"
-
+#include "system.h"
+#include "io.h"
+#include "unistd.h"
 int main(void)
 { 
   alt_putstr("Hello from Nios II!\n");
+  volatile int count = 0x2142FF;
+  IOWR_32DIRECT(DISPLAY_SEGMENT_0_BASE, 4, 41500);
+  IOWR_32DIRECT(DISPLAY_SEGMENT_0_BASE, 8, 50000);
+  IOWR_32DIRECT(DISPLAY_SEGMENT_0_BASE, 0, count);
 
   /* Event loop never exits. */
-  while (1);
+  while (1){
+
+	  IOWR_32DIRECT(DISPLAY_SEGMENT_0_BASE, 0, count);
+	  usleep(10);
+	  count++;
+  }
 
   return 0;
 }
