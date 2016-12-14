d1a74c7b6fcced090101b8b034256286e65390f2
pokitoz <florian.depraz@epfl.ch>
2016-12-14 13:20:45 +0100

    Applciation helloworld NiosII

---
+++ sw/nios/application/Display_Segment/obj/default/hello_world_small.d
@@ -0,0 +1,4 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ /home/pokitoz/Desktop/DE0_SoC_LedRing/sw/nios/application/Display_Segment_bsp//HAL/inc/sys/alt_stdio.h
+
+/home/pokitoz/Desktop/DE0_SoC_LedRing/sw/nios/application/Display_Segment_bsp//HAL/inc/sys/alt_stdio.h:
