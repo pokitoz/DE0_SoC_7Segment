f9e1414dc37a5fd8d401ccb3ccf3719acfdab14c
pokitoz <florian.depraz@epfl.ch>
2016-12-14 13:04:38 +0100

    Remove autogenerated folder

--- hw/quartus/system_NiosII/system_NiosII_inst.v
+++
@@ -1,8 +0,0 @@
-	system_NiosII u0 (
-		.clk_clk                                          (<connected-to-clk_clk>),                                          //                         clk.clk
-		.display_segment_0_nseldig_readdata               (<connected-to-display_segment_0_nseldig_readdata>),               //   display_segment_0_nseldig.readdata
-		.display_segment_0_reset_led_writeresponsevalid_n (<connected-to-display_segment_0_reset_led_writeresponsevalid_n>), // display_segment_0_reset_led.writeresponsevalid_n
-		.display_segment_0_selseg_readdata                (<connected-to-display_segment_0_selseg_readdata>),                //    display_segment_0_selseg.readdata
-		.reset_reset_n                                    (<connected-to-reset_reset_n>)                                     //                       reset.reset_n
-	);
-
